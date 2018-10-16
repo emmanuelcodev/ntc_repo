38124a15ecd863e86390c002169021228b0bb1d5
emmanuelcodev <emmanuel.adw@gmail.com>
2018-10-16 15:35:34 -0700

    f

--- ntc_project/market/views.py
+++ ntc_project/market/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 import os
 # Create your views here.
 def test_index(request):
-    return render(request, os.path.join('market', 'market.html'))
+    return render(request, os.path.join('market', 'market.html'), {})
