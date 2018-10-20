f80efead56ed3cdf62cd7e96c3a2bc32d04398da
emmanuelcodev <emmanuel.adw@gmail.com>
2018-10-20 13:23:42 -0700

    Added project level static and template files for base and header. Header non reponsive. Market app set up , responsive not set up to database. Database created

--- ntc_project/market/views.py
+++ ntc_project/market/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import os
 # Create your views here.
 def test_index(request):
-    return render(request, os.path.join('market', 'market.html'), {})
+    return render(request, os.path.join('market', 'market.html'), {'market':'market'})
+
+
+def notes_that_match_category(request, category_slug = None):
+
+    category = None
+    notes = None
+
+    if category_slug!=None:
+        category  = get_object_or_404(Category, cat_slug = category_slug)
+        notes = Notes.objects.filter(note_category = category)#get notes that match a specific category
+    else:
+        notes = Notes.objects.all() #get all notes
+
+    return render(request, os.path.join('market', 'market.html'), {'market':'market', 'category':category, 'notes':notes})
