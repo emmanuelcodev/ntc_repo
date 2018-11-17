486f52af9b991a995683c326645445c2c756612d
emmanuelcodev <emmanuel.adw@gmail.com>
2018-11-17 13:23:34 -0800

    Added cart functionality so only allows cart item to be added once. Cart models created, views redirects you to cart page. Fixed comment bar, rating number bar in the note preview pages.

--- ntc_project/cart/admin.py
+++ ntc_project/cart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import CartItems
 # Register your models here.
+
+
+class CartItemsManager(admin.ModelAdmin):
+    list_display = ['id']
+admin.site.register(CartItems, CartItemsManager)
--- ntc_project/cart/models.py
+++ ntc_project/cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from market.models import Notes
 # Create your models here.
+
+
+class CartItems(models.Model):
+    note = models.ForeignKey(Notes, on_delete=models.CASCADE, unique=True)
+
+    def cart_item_id(request):
+        return id
