08542968b1c40dc7019a73608fd73a059477a209
emmanuelcodev <emmanuel.adw@gmail.com>
2018-11-19 12:03:53 -0800

    fixed add to cart, remove to cart so it dynamically loads data from database fixed bug where it would load it but now show it.

--- ntc_project/cart/urls.py
+++ ntc_project/cart/urls.py
@@ -1,9 +1,9 @@
 
 from django.urls import path, include
 from . import views
-app_name = "cart"
+app_name = 'cart'
 urlpatterns = [
-    path('', views.cart,name="cart"),
+    path('', views.cart,name='cart'),
     path('<int:product_id>/', views.add_to_cart, name = 'add_to_cart'),
     path('<int:cart_id>/<str:dummy>/', views.remove_cart, name= 'remove_cart'),
 ]
