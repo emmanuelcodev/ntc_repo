2e2df3c432c6ea2392a3a97dbd676bc3c9f105e3
emmanuelcodev <emmanuel.adw@gmail.com>
2018-11-22 11:50:40 -0800

    changed database types to postgres

--- ntc_project/ntc/settings.py
+++ ntc_project/ntc/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'ntc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ntc',
+        'USER': 'postgres',
+        'PASSWORD':'django1234',
+        'HOST':'localhost',
+        'PORT':'5432',
+
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
